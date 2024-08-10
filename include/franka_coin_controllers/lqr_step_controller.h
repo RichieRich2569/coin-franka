9037fd2f61b73ee8bd1f6397bd333037bec56cda
RichieRich2569 <rm967@cam.ac.uk>
2024-08-10 15:19:07 +0100

    Changed LQR control to cartesian velocity based

--- include/franka_coin_controllers/lqr_step_controller.h
+++ include/franka_coin_controllers/lqr_step_controller.h
@@ -3,69 +3,35 @@
 #pragma once
 
 #include <memory>
-#include <mutex>
 #include <string>
-#include <vector>
 
 #include <controller_interface/multi_interface_controller.h>
-#include <dynamic_reconfigure/server.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <hardware_interface/joint_command_interface.h>
+#include <franka_hw/franka_cartesian_command_interface.h>
+#include <franka_hw/franka_state_interface.h>
 #include <hardware_interface/robot_hw.h>
 #include <ros/node_handle.h>
 #include <ros/time.h>
-#include <Eigen/Dense>
-
-#include <franka_coin_controllers/compliance_paramConfig.h>
-#include <franka_hw/franka_model_interface.h>
-#include <franka_hw/franka_state_interface.h>
 
 namespace franka_coin_controllers {
 
 class LQRStepController : public controller_interface::MultiInterfaceController<
-                                                franka_hw::FrankaModelInterface,
-                                                hardware_interface::EffortJointInterface,
-                                                franka_hw::FrankaStateInterface> {
+                                               franka_hw::FrankaVelocityCartesianInterface,
+                                               franka_hw::FrankaStateInterface,
+                                               franka_hw::FrankaModelInterface> {
  public:
-  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle) override;
-  void starting(const ros::Time&) override;
+  bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
   void update(const ros::Time&, const ros::Duration& period) override;
+  void starting(const ros::Time&) override;
+  void stopping(const ros::Time&) override;
 
  private:
-  // Saturation
-  Eigen::Matrix<double, 7, 1> saturateTorqueRate(
-      const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
-      const Eigen::Matrix<double, 7, 1>& tau_J_d);  // NOLINT (readability-identifier-naming)
-
+  franka_hw::FrankaVelocityCartesianInterface* velocity_cartesian_interface_;
+  std::unique_ptr<franka_hw::FrankaCartesianVelocityHandle> velocity_cartesian_handle_;
   std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
   std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
-  std::vector<hardware_interface::JointHandle> joint_handles_;
+  ros::Duration elapsed_time_;
 
-  double filter_params_{0.005};
-  double nullspace_stiffness_{20.0};
-  double nullspace_stiffness_target_{20.0};
-  const double delta_tau_max_{1.0};
-  Eigen::Matrix<double, 6, 6> cartesian_stiffness_;
-  Eigen::Matrix<double, 6, 6> cartesian_stiffness_target_;
-  Eigen::Matrix<double, 6, 6> cartesian_damping_;
-  Eigen::Matrix<double, 6, 6> cartesian_damping_target_;
-  Eigen::Matrix<double, 7, 1> q_d_nullspace_;
-  Eigen::Vector3d position_d_;
-  Eigen::Quaterniond orientation_d_;
-  std::mutex position_and_orientation_d_target_mutex_;
-  Eigen::Vector3d position_d_target_;
-  Eigen::Quaterniond orientation_d_target_;
-
-  // Dynamic reconfigure
-  std::unique_ptr<dynamic_reconfigure::Server<franka_coin_controllers::compliance_paramConfig>>
-      dynamic_server_compliance_param_;
-  ros::NodeHandle dynamic_reconfigure_compliance_param_node_;
-  void complianceParamCallback(franka_coin_controllers::compliance_paramConfig& config,
-                               uint32_t level);
-
-  // Equilibrium pose subscriber
-  ros::Subscriber sub_equilibrium_pose_;
-  void equilibriumPoseCallback(const geometry_msgs::PoseStampedConstPtr& msg);
+  Eigen::Matrix<double, 6, 1> state_k_; // Current state vector for LQR controller with Integral Control
 };
 
 }  // namespace franka_coin_controllers
--- src/lqr_step_controller.cpp
+++ src/lqr_step_controller.cpp
@@ -2,37 +2,41 @@
 // Use of this source code is governed by the Apache-2.0 license, see LICENSE
 #include <franka_coin_controllers/lqr_step_controller.h>
 
+#include <array>
 #include <cmath>
 #include <memory>
+#include <string>
 
 #include <controller_interface/controller_base.h>
-#include <franka/robot_state.h>
+#include <hardware_interface/hardware_interface.h>
+#include <hardware_interface/joint_command_interface.h>
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
 
-#include <franka_coin_controllers/pseudo_inversion.h>
-
 namespace franka_coin_controllers {
 
-bool LQRStepController::init(hardware_interface::RobotHW* robot_hw,
-                                               ros::NodeHandle& node_handle) {
-  std::vector<double> cartesian_stiffness_vector;
-  std::vector<double> cartesian_damping_vector;
-
-  sub_equilibrium_pose_ = node_handle.subscribe(
-      "equilibrium_pose", 20, &LQRStepController::equilibriumPoseCallback, this,
-      ros::TransportHints().reliable().tcpNoDelay());
-
+bool LQRStepController::init(hardware_interface::RobotHW* robot_hardware,
+                                              ros::NodeHandle& node_handle) {
   std::string arm_id;
   if (!node_handle.getParam("arm_id", arm_id)) {
-    ROS_ERROR_STREAM("LQRStepController: Could not read parameter arm_id");
+    ROS_ERROR("LQRStepController: Could not get parameter arm_id");
     return false;
   }
-  std::vector<std::string> joint_names;
-  if (!node_handle.getParam("joint_names", joint_names) || joint_names.size() != 7) {
+
+  velocity_cartesian_interface_ =
+      robot_hardware->get<franka_hw::FrankaVelocityCartesianInterface>();
+  if (velocity_cartesian_interface_ == nullptr) {
     ROS_ERROR(
-        "LQRStepController: Invalid or no joint_names parameters provided, "
-        "aborting controller init!");
+        "LQRStepController: Could not get Cartesian velocity interface from "
+        "hardware");
+    return false;
+  }
+  try {
+    velocity_cartesian_handle_ = std::make_unique<franka_hw::FrankaCartesianVelocityHandle>(
+        velocity_cartesian_interface_->getHandle(arm_id + "_robot"));
+  } catch (const hardware_interface::HardwareInterfaceException& e) {
+    ROS_ERROR_STREAM(
+        "LQRStepController: Exception getting Cartesian handle: " << e.what());
     return false;
   }
 
@@ -52,217 +56,100 @@ bool LQRStepController::init(hardware_interface::RobotHW* robot_hw,
     return false;
   }
 
-  auto* state_interface = robot_hw->get<franka_hw::FrankaStateInterface>();
+  auto state_interface = robot_hardware->get<franka_hw::FrankaStateInterface>();
   if (state_interface == nullptr) {
-    ROS_ERROR_STREAM(
-        "LQRStepController: Error getting state interface from hardware");
+    ROS_ERROR("LQRStepController: Could not get state interface from hardware");
     return false;
   }
+
   try {
-    state_handle_ = std::make_unique<franka_hw::FrankaStateHandle>(
-        state_interface->getHandle(arm_id + "_robot"));
+    state_handle_ = state_interface->getHandle(arm_id + "_robot");
 
     std::array<double, 7> q_start{{0, 0, 0, -7 * M_PI_4 / 2, 0, 7 * M_PI_4 / 2, 0}};
     for (size_t i = 0; i < q_start.size(); i++) {
-      if (std::abs(state_handle_->getRobotState().q_d[i] - q_start[i]) > 0.1) {
+      if (std::abs(state_handle_.getRobotState().q_d[i] - q_start[i]) > 0.1) {
         ROS_ERROR_STREAM(
-            "LQRStepController: Robot is not in the expected starting position for "
-            "running this example. Run `roslaunch franka_coin_controllers coin_move_to_start.launch "
-            "robot_ip:=<robot-ip> load_gripper:=<has-attached-gripper>` first.");
+            "LQRStepController: Robot is not in the expected starting position "
+            "for running this example. Run `roslaunch franka_coin_controllers "
+            "coin_move_to_start.launch robot_ip:=<robot-ip> load_gripper:=<has-attached-gripper>` "
+            "first.");
         return false;
       }
     }
-  } catch (hardware_interface::HardwareInterfaceException& ex) {
+  } catch (const hardware_interface::HardwareInterfaceException& e) {
     ROS_ERROR_STREAM(
-        "LQRStepController: Exception getting state handle from interface: "
-        << ex.what());
+        "LQRStepController: Exception getting state handle: " << e.what());
     return false;
   }
 
-  
-
-  auto* effort_joint_interface = robot_hw->get<hardware_interface::EffortJointInterface>();
-  if (effort_joint_interface == nullptr) {
-    ROS_ERROR_STREAM(
-        "LQRStepController: Error getting effort joint interface from hardware");
-    return false;
-  }
-  for (size_t i = 0; i < 7; ++i) {
-    try {
-      joint_handles_.push_back(effort_joint_interface->getHandle(joint_names[i]));
-    } catch (const hardware_interface::HardwareInterfaceException& ex) {
-      ROS_ERROR_STREAM(
-          "LQRStepController: Exception getting joint handles: " << ex.what());
-      return false;
-    }
-  }
-
-  dynamic_reconfigure_compliance_param_node_ =
-      ros::NodeHandle(node_handle.getNamespace() + "/dynamic_reconfigure_compliance_param_node");
-
-  dynamic_server_compliance_param_ = std::make_unique<
-      dynamic_reconfigure::Server<franka_coin_controllers::compliance_paramConfig>>(
-
-      dynamic_reconfigure_compliance_param_node_);
-  dynamic_server_compliance_param_->setCallback(
-      boost::bind(&LQRStepController::complianceParamCallback, this, _1, _2));
-
-  position_d_.setZero();
-  orientation_d_.coeffs() << 0.0, 0.0, 0.0, 1.0;
-  position_d_target_.setZero();
-  orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
-
-  cartesian_stiffness_.setZero();
-  cartesian_damping_.setZero();
+  state_k_.setZero();
 
   return true;
 }
 
-void LQRStepController::starting(const ros::Time& /*time*/) {
-  // compute initial velocity with jacobian and set x_attractor and q_d_nullspace
-  // to initial configuration
+void LQRStepController::starting(const ros::Time& /* time */) {
+  elapsed_time_ = ros::Duration(0.0);
   franka::RobotState initial_state = state_handle_->getRobotState();
-  // get jacobian
   std::array<double, 42> jacobian_array =
-      model_handle_->getZeroJacobian(franka::Frame::kEndEffector);
+      model_handle_->getZeroJacobian(franka::Frame::kEndEffector)
+
   // convert to eigen
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> q_initial(initial_state.q.data());
   Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
-
-  // set equilibrium point to current state
-  position_d_ = initial_transform.translation();
-  orientation_d_ = Eigen::Quaterniond(initial_transform.rotation());
-  position_d_target_ = initial_transform.translation();
-  orientation_d_target_ = Eigen::Quaterniond(initial_transform.rotation());
-
-  // set nullspace equilibrium configuration to initial q
-  q_d_nullspace_ = q_initial;
-}
-
-void LQRStepController::update(const ros::Time& /*time*/,
-                                                 const ros::Duration& /*period*/) {
-  // get state variables
-  franka::RobotState robot_state = state_handle_->getRobotState();
-  std::array<double, 7> coriolis_array = model_handle_->getCoriolis();
-  std::array<double, 42> jacobian_array =
-      model_handle_->getZeroJacobian(franka::Frame::kEndEffector);
-
-  // convert to Eigen
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
   Eigen::Map<Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
   Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_J_d(  // NOLINT (readability-identifier-naming)
-      robot_state.tau_J_d.data());
-  Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
-  Eigen::Vector3d position(transform.translation());
-  Eigen::Quaterniond orientation(transform.rotation());
 
-  // Compute Cartesian velocity
-  Eigen::Matrix<double, 6, 1> cartesian_velocity = jacobian * dq;
-  Eigen::Vector3d velocity = cartesian_velocity.head<3>();
+  // get initial state
+  Eigen::Vector3d position_k = initial_transform.translation();
+  Eigen::Matrix<double, 6, 1> velocity_k = jacobian * dq;
 
-  // Display end-effector mass
-  // Extract the end-effector mass (m_ee)
-  double m_ee = robot_state.m_total;
+  state_k_.segment<2>(0) = position_k.head<2>();
+  state_k_.segment<2>(2) = velocity_k.head<2>();
 
-  // Print the end-effector mass to the terminal
-  std::cout << "End-Effector Mass (m_ee): " << m_ee << " kg" << std::endl;
-
-  // compute error to desired pose
-  // position error
-  Eigen::Matrix<double, 6, 1> error;
-  error.head(3) << position - position_d_;
-
-  // orientation error
-  if (orientation_d_.coeffs().dot(orientation.coeffs()) < 0.0) {
-    orientation.coeffs() << -orientation.coeffs();
-  }
-  // "difference" quaternion
-  Eigen::Quaterniond error_quaternion(orientation.inverse() * orientation_d_);
-  error.tail(3) << error_quaternion.x(), error_quaternion.y(), error_quaternion.z();
-  // Transform to base frame
-  error.tail(3) << -transform.rotation() * error.tail(3);
-
-  // compute control
-  // allocate variables
-  Eigen::VectorXd tau_task(7), tau_nullspace(7), tau_d(7);
-
-  // pseudoinverse for nullspace handling
-  // kinematic pseuoinverse
-  Eigen::MatrixXd jacobian_transpose_pinv;
-  pseudoInverse(jacobian.transpose(), jacobian_transpose_pinv);
-
-  // Cartesian PD control with damping ratio = 1
-  tau_task << jacobian.transpose() *
-                  (-cartesian_stiffness_ * error - cartesian_damping_ * (jacobian * dq));
-  // nullspace PD control with damping ratio = 1
-  tau_nullspace << (Eigen::MatrixXd::Identity(7, 7) -
-                    jacobian.transpose() * jacobian_transpose_pinv) *
-                       (nullspace_stiffness_ * (q_d_nullspace_ - q) -
-                        (2.0 * sqrt(nullspace_stiffness_)) * dq);
-  // Desired torque
-  tau_d << tau_task + tau_nullspace + coriolis;
-  // Saturate torque rate to avoid discontinuities
-  tau_d << saturateTorqueRate(tau_d, tau_J_d);
-  for (size_t i = 0; i < 7; ++i) {
-    joint_handles_[i].setCommand(tau_d(i));
-  }
-
-  // update parameters changed online either through dynamic reconfigure or through the interactive
-  // target by filtering
-  cartesian_stiffness_ =
-      filter_params_ * cartesian_stiffness_target_ + (1.0 - filter_params_) * cartesian_stiffness_;
-  cartesian_damping_ =
-      filter_params_ * cartesian_damping_target_ + (1.0 - filter_params_) * cartesian_damping_;
-  nullspace_stiffness_ =
-      filter_params_ * nullspace_stiffness_target_ + (1.0 - filter_params_) * nullspace_stiffness_;
-  std::lock_guard<std::mutex> position_d_target_mutex_lock(
-      position_and_orientation_d_target_mutex_);
-  position_d_ = filter_params_ * position_d_target_ + (1.0 - filter_params_) * position_d_;
-  orientation_d_ = orientation_d_.slerp(filter_params_, orientation_d_target_);
 }
 
-Eigen::Matrix<double, 7, 1> LQRStepController::saturateTorqueRate(
-    const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
-    const Eigen::Matrix<double, 7, 1>& tau_J_d) {  // NOLINT (readability-identifier-naming)
-  Eigen::Matrix<double, 7, 1> tau_d_saturated{};
-  for (size_t i = 0; i < 7; i++) {
-    double difference = tau_d_calculated[i] - tau_J_d[i];
-    tau_d_saturated[i] =
-        tau_J_d[i] + std::max(std::min(difference, delta_tau_max_), -delta_tau_max_);
-  }
-  return tau_d_saturated;
+void LQRStepController::update(const ros::Time& /* time */,
+                                                const ros::Duration& period) {
+  elapsed_time_ += period;
+  franka::RobotState initial_state = state_handle_->getRobotState();
+  std::array<double, 42> jacobian_array =
+      model_handle_->getZeroJacobian(franka::Frame::kEndEffector)
+
+  // convert to eigen
+  Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
+  Eigen::Map<Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
+
+  double time_max = 5.0;
+  double v_max = 0.5; // Limit for Panda is 1.7 m/s
+
+  // get new state
+  Eigen::Vector3d position_k = initial_transform.translation();
+  Eigen::Matrix<double, 6, 1> velocity_k = jacobian * dq;
+
+  state_k_.segment<2>(0) = position_k.head<2>();
+  state_k_.segment<2>(2) = velocity_k.head<2>();
+  state_k_(4) = state_k_(4) + (position_k(0) - 1.0)*period; // Track reference of 1
+  state_k_(5) = position_k(0) - 1.0;
+
+  // calculate LQR gain
+
+  // Find appropriate velocities
+
+
+  double angle = M_PI / 4.0;
+  double cycle = std::floor(
+      pow(-1.0, (elapsed_time_.toSec() - std::fmod(elapsed_time_.toSec(), time_max)) / time_max));
+  double v = cycle * v_max / 2.0 * (1.0 - std::cos(2.0 * M_PI / time_max * elapsed_time_.toSec()));
+  double v_x = std::cos(angle) * v;
+  double v_z = -std::sin(angle) * v;
+  std::array<double, 6> command = {{v_x, 0.0, v_z, 0.0, 0.0, 0.0}};
+  velocity_cartesian_handle_->setCommand(command);
 }
 
-void LQRStepController::complianceParamCallback(
-    franka_coin_controllers::compliance_paramConfig& config,
-    uint32_t /*level*/) {
-  cartesian_stiffness_target_.setIdentity();
-  cartesian_stiffness_target_.topLeftCorner(3, 3)
-      << config.translational_stiffness * Eigen::Matrix3d::Identity();
-  cartesian_stiffness_target_.bottomRightCorner(3, 3)
-      << config.rotational_stiffness * Eigen::Matrix3d::Identity();
-  cartesian_damping_target_.setIdentity();
-  // Damping ratio = 1
-  cartesian_damping_target_.topLeftCorner(3, 3)
-      << 2.0 * sqrt(config.translational_stiffness) * Eigen::Matrix3d::Identity();
-  cartesian_damping_target_.bottomRightCorner(3, 3)
-      << 2.0 * sqrt(config.rotational_stiffness) * Eigen::Matrix3d::Identity();
-  nullspace_stiffness_target_ = config.nullspace_stiffness;
-}
-
-void LQRStepController::equilibriumPoseCallback(
-    const geometry_msgs::PoseStampedConstPtr& msg) {
-  std::lock_guard<std::mutex> position_d_target_mutex_lock(
-      position_and_orientation_d_target_mutex_);
-  position_d_target_ << msg->pose.position.x, msg->pose.position.y, msg->pose.position.z;
-  Eigen::Quaterniond last_orientation_d_target(orientation_d_target_);
-  orientation_d_target_.coeffs() << msg->pose.orientation.x, msg->pose.orientation.y,
-      msg->pose.orientation.z, msg->pose.orientation.w;
-  if (last_orientation_d_target.coeffs().dot(orientation_d_target_.coeffs()) < 0.0) {
-    orientation_d_target_.coeffs() << -orientation_d_target_.coeffs();
-  }
+void LQRStepController::stopping(const ros::Time& /*time*/) {
+  // WARNING: DO NOT SEND ZERO VELOCITIES HERE AS IN CASE OF ABORTING DURING MOTION
+  // A JUMP TO ZERO WILL BE COMMANDED PUTTING HIGH LOADS ON THE ROBOT. LET THE DEFAULT
+  // BUILT-IN STOPPING BEHAVIOR SLOW DOWN THE ROBOT.
 }
 
 }  // namespace franka_coin_controllers
